d8392294bbe4776c0fd271767b48f7f4ac5824c5
Marcus <davi2812@dee.ufc.br>
2020-02-03 01:39:14 -0300

    more tests, added matlab script

--- src/normals.cpp
+++ src/normals.cpp
@@ -23,37 +23,35 @@ int main()
 
   // Create the normal estimation class, and pass the input dataset to it
   pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> ne;
-  ne.setInputCloud (cloud);
-
-  // Create an empty kdtree representation, and pass it to the normal estimation object.
-  // Its content will be filled inside the object, based on the given input dataset (as no other search surface is given).
+  pcl::PointCloud<pcl::Normal>::Ptr normals (new pcl::PointCloud<pcl::Normal>);
   pcl::search::KdTree<pcl::PointXYZ>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZ> ());
+//  tree->setInputCloud(cloud);
+  ne.setInputCloud (cloud);
   ne.setSearchMethod (tree);
-
-  // Output datasets
-  pcl::PointCloud<pcl::Normal>::Ptr cloud_normals (new pcl::PointCloud<pcl::Normal>);
-
-  // Use all neighbors in a sphere of radius 3cm
-  ne.setRadiusSearch (0.5);
-
-  // Compute the features
-  ne.compute (*cloud_normals);
+//  ne.setKSearch(20);
+  ne.setRadiusSearch (0.2);
+  ne.compute (*normals);
+  
 
   float nx = 0 ,ny = 0 ,nz = 0;
 	
-	std::cout << "size = " << cloud_normals->size() << std::endl;
-	for(int i=0;i<cloud_normals->size();++i){
+	std::cout << "size = " << normals->size() << std::endl;
+	for(int i=0;i<normals->size();++i){
+//	std::cout << "x normal = " << normals->points[i].normal_x << std::endl;
 
-	nx += cloud_normals->points[i].normal_x;
-	ny += cloud_normals->points[i].normal_y;
-	nz += cloud_normals->points[i].normal_y;
+	if(!std::isnan(normals->points[i].normal_x))
+	nx += (normals->points[i].normal_x);
+	if(!std::isnan(normals->points[i].normal_y))
+	ny += (normals->points[i].normal_y);
+	if(!std::isnan(normals->points[i].normal_z))
+	nz += (normals->points[i].normal_z);
 
 	}
 
 	std::cout << "area = " << nx << " " << ny << " " << nz << std::endl;
 
   pcl::PointCloud<pcl::PointNormal>::Ptr cloud_out(new pcl::PointCloud<pcl::PointNormal>);
- pcl::concatenateFields(*cloud,*cloud_normals,*cloud_out);
+ pcl::concatenateFields(*cloud,*normals,*cloud_out);
  
 	  pcl::io::savePCDFileASCII(WORKINGPATH "normals_output_ascii.pcd",*cloud_out);
   pcl::io::savePCDFileBinary(WORKINGPATH "normals_output_bin.pcd",*cloud_out);
